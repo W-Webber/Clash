e774cfab4c763d16ab165555b2c0a1fdc57e4b02
Webber <webber_lee@outlook.com>
2025-11-28 19:45:14 +0800

    test

---
+++ list/clear_cache.js
@@ -0,0 +1,18 @@
+//const remoteUrl = "https://raw.githubusercontent.com/W-Webber/jd_surge/refs/heads/feature-qx/config_helper.js";
+const remoteUrl = "https://raw.githubusercontent.com/W-Webber/Clash/refs/heads/master/list/remote_test.js"
+
+const $argument = "clear-cache";
+
+(async () => {
+  try {
+    const resp = await $task.fetch({ url: remoteUrl });
+    console.log("============ begin ===========");
+    eval(resp.body);  // 远程脚本中可以直接用 REMOTE_ARG 这个变量
+    console.log("============ end  ===========");
+    //console.log(resp.body);
+    //$done();
+  } catch (e) {
+    $notify("远程脚本加载失败", "", String(e));
+    $done();
+  }
+})();
